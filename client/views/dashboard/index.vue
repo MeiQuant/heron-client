802af2f2556d85881471950ba308e90d9f1d4590
onthedesk <xiuguozhao@gmail.com>
2016-12-06 22:34:31 +0800

    bind event by @

--- client/views/dashboard/index.vue
+++ client/views/dashboard/index.vue
@@ -7,10 +7,10 @@
           <p class="subtitle">管理服务器与CTP柜台的通信</p>
           <div class="control is-grouped">
             <p class="control">
-              <a class="button is-success">启动服务</a>
+              <a class="button is-success" @click="startService">启动服务</a>
             </p>
             <p class="control">
-              <a class="button is-danger">关闭服务</a>
+              <a class="button is-danger" @click="stopService">关闭服务</a>
             </p>
           </div>
         </div>
@@ -18,9 +18,9 @@
         <div class="box">
           <p class="title">系统状态</p>
           <p class="subtitle">服务器当前运行状态</p>
-          <p class="h1">CPU</p>
+          <p class="h1">CPU 15%</p>
           <progress class="progress is-primary" value="15" max="100">15%</progress>
-          <p class="h1">内存</p>
+          <p class="h1">内存 35%</p>
           <progress class="progress is-info" value="35" max="100">15%</progress>
         </div>
 
@@ -43,14 +43,33 @@
 import Vue from 'vue'
 import {VueSocketio as Socket} from 'vue-socket.io'
 
-import store from '../../store'
-
 export default {
+
+  created: () => {
+    //实例创建之后， 建立与服务端的链接
+    //alert('我被创建了')
+
+    Vue.use(Socket, '');
+
+  },
+
   computed: {
     log() {
       //返回日志数据, 根据类别，处理样式
-      return store.state.logs[0]
+      return this.$store.state.logs[0]
     }
+  },
+
+  methods: {
+
+    startService (e) {
+      // alert('我要启动服务')
+    },
+
+    stopService (e) {
+      alert('小哥，把服务关掉')
+    }
+
   }
 }
 </script>
